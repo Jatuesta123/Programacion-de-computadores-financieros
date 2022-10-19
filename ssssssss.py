b723c151a5b82613f3fdeed24732801538670825
CSU21-AINF <CSU21@academico.unab.ad>
2022-10-19 17:33:00 -0500

    mi primer proyecto

---
+++ ssssssss.py
@@ -0,0 +1,15 @@
+class helado:
+    def __init__(self,sabor,bolas,color,toppings):
+         self.sabor=sabor
+         self.bolas=bolas
+         self.color= color
+         self.toppings=toppings
+p1=helado('chicle',1,'azul',"cereza")
+
+print(p1.sabor)
+print(p1.bolas)
+print(p1.color)
+print(p1.toppings)
+
+
+   
